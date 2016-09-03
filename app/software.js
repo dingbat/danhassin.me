917e1b197f92a4a15250f46719b0e8d9832b8745
Dan Hassin <danhassin@mac.com>
2016-09-03 16:48:47 -0400

    Small projects

--- app/software.js
+++ app/software.js
@@ -54,10 +54,27 @@ const BigItems = [
   },
 
   {
+    name: "probabilistic programming",
+    img: "ide.png",
+    link: "cs200/index.html",
+    links: {
+      live: "cs200/index.html",
+      paper: "cs200/paper.pdf",
+      source: "https://github.com/RocHack/jschain",
+    },
+    tech: 'js, node.js, escodegen, esprima',
+    description: () =>
+      <p>
+        proof of concept system for generating javascript programs and code snippets based off 
+        a corpus of source files, using a <a href="https://en.wikipedia.org/wiki/Markov_chain">markov-chain</a> mechanism for tree structures.
+      </p>
+  },
+
+  {
     name: "nsrails",
     img: "iror.png",
     noBorder: true,
-    link: "http://www.sightunseen.audio",
+    link: "https://github.com/dingbat/nsrails",
     links: {
       source: "https://github.com/dingbat/nsrails",
       site: "http://nsrails.com",
@@ -69,16 +86,96 @@ const BigItems = [
         objective-c framework for high-level, easy, and flexible communication with a RESTful back-end (with rails conventions).
       </p>
   },
+]
+
+const SmallItems = [
+  {
+    name: "crm114_native",
+    link: "https://github.com/dingbat/crm114_native",
+    links: {
+      source: "https://github.com/dingbat/crm114_native",
+    },
+    tech: 'ruby gem, ruby-C bindings',
+    description: () =>
+      <p>
+        a native ruby wrapper around <a href="http://crm114.sourceforge.net/wiki/doku.php">CRM114</a>,
+        a machine learning library in C for text classification.
+      </p>
+  },
+
+  {
+    name: "clockout",
+    img: "clockout.png",
+    noBorder: true,
+    link: "https://github.com/dingbat/clockout",
+    links: {
+      source: "https://github.com/dingbat/crm114_native",
+    },
+    tech: 'ruby gem/executable, git hook',
+    description: () =>
+      <p>
+        tool that analyzes a git repository and converts your commits into blocks of time worked
+        (useful as a replacement/addition to contract timekeeping)
+      </p>
+  },
+
+  {
+    name: "blarfb",
+    img: "blarfb.jpg",
+    link: "https://chrome.google.com/webstore/detail/blarfb/japabiflacoedddenbhneajhepopkdif",
+    links: {
+      chrome: "https://chrome.google.com/webstore/detail/blarfb/japabiflacoedddenbhneajhepopkdif",
+      safari: "stuff/blarfb.safariextz",
+      source: "https://github.com/dingbat/blarfb",
+    },
+    tech: 'chrome, safari extension',
+    description: () =>
+      <p>
+        got the idly-scroll-fb-when-there's-stuff-to-be-done disease? this may help
+      </p>
+  },
+
+  {
+    name: "'memoring'",
+    img: "memoring.jpg",
+    link: "https://chrome.google.com/webstore/detail/blarfb/japabiflacoedddenbhneajhepopkdif",
+    links: {
+      'hackny standings': "http://hackny.org/a/2013/10/fall-2013-hackny-hackathon-recap-and-winners/",
+    },
+    tech: 'ios, shapeways API, php',
+    description: () =>
+      <div><p>
+        a 24-hour hack, done for the fall 2013 HackNY Hackathon in NYC, my first hackathon.
+        tracks your location throughout a night, and allows you to order a 3d-printed ring in the shape of your walk.
+      </p>
+      <p>
+        won 2nd place overall, among around 250 competitors and 50 other projects, and was presented at the november 2013 NY Tech Meetup.
+      </p></div>
+  },
+
+  {
+    name: "λ-calculus interpreter",
+    img: "lambda.png",
+    link: "lambda",
+    links: {
+    },
+    tech: 'js',
+    description: () =>
+      <p>
+        an interpreter for <a href="http://en.wikipedia.org/wiki/Lambda_calculus">the lambda calculus</a>
+      </p>
+  },
   
 ]
-const SmallItems = []
 
 export default class Software extends Component {
   renderItem(item) {
     return (
       <div key={item.name} className="item">
         <a href={item.link}>
-          <img className={item.noBorder && 'no-border'} src={require(`../img/s/${item.img}`)} />
+          {item.img &&
+            <img className={item.noBorder && 'no-border'} src={require(`../img/s/${item.img}`)} />
+          }
           <h3>{item.name}</h3>
         </a>
         {item.description()}
