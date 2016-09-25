f68a2fd596cbff381386dbd7f361d9bcf1fd6bd0
Dan Hassin <danhassin@mac.com>
2016-09-25 15:09:48 -0400

    add two more religion papers

---
+++ app/papers.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export const papers = [
+  {
+    title: "“Skedge” – Honors senior thesis",
+    url: "https://dropbox.com/s/dg0zic5376d7re4/thesis.pdf?raw=1",
+    year: "2016",
+  },
+  {
+    title: "“Advice & Dissent”",
+    url: "papers/ad.pdf",
+    year: "2015",
+  },
+  {
+    title: "“Theories of Religion” Portfolio – A case study on Zazen",
+    url: "papers/zen-portfolio.pdf",
+    year: "2015",
+  },
+  {
+    title: "“The Memetic Cigarette”",
+    url: "papers/cig.pdf",
+    year: "2015",
+  },
+  {
+    title: () => (~
+      .
+        “Between Two Worlds: Mysticism in
+        %span.italics
+           Four Quartets
+        ”
+    ~),
+    url: "papers/eliot.pdf",
+    year: "2014",
+  },
+  {
+    title: "“You are what you read” – Analysis of Jewish scripture-eating",
+    url: "papers/jewish-eating.pdf",
+    year: "2016",
+  },
+  {
+    title: "“Non-Duality” – A study of Zen philosophy",
+    url: "papers/zen-nonduality.pdf",
+    year: "2014",
+  },
+  {
+    title: "Refutation of a proof of P=NP",
+    url: "http://arxiv.org/abs/1404.5352",
+    year: "2014",
+  },
+]
+
+export const other = [
+  {
+    title: "twitter",
+    url: "http://twitter.com/2nf340",
+  },
+  {
+    title: "2nf340",
+    url: "http://2nf340.danhassin.me",
+  },
+  {
+    title: "how i felt in the summer of 2013",
+    url: "stuff/poems.pdf",
+  },
+]
--- app/writing.js
+++ app/writing.js
@@ -3,60 +3,8 @@ import { Link } from 'react-router';
 
 import 'style/writing.sass';
 
-const papers = [
-  {
-    title: "“Skedge” – Honors senior thesis",
-    url: "https://dropbox.com/s/dg0zic5376d7re4/thesis.pdf?raw=1",
-    year: "2016",
-  },
-  {
-    title: "“Advice & Dissent”",
-    url: "papers/ad.pdf",
-    year: "2015",
-  },
-  {
-    title: "“The Memetic Cigarette”",
-    url: "papers/cig.pdf",
-    year: "2015",
-  },
-  {
-    title: () => (~
-      .
-        “Between Two Worlds: Mysticism in 
-        %span.italics Four Quartets
-        ”
-    ~),
-    url: "papers/eliot.pdf",
-    year: "2014",
-  },
-  {
-    title: "“Non-Duality” – A study of Zen philosophy”",
-    url: "papers/zen-nonduality.pdf",
-    year: "2014",
-  },
-  {
-    title: "Refutation of a proof of P=NP",
-    url: "http://arxiv.org/abs/1404.5352",
-    year: "2014",
-  },
-]
-
-const other = [ 
-  {
-    title: "twitter",
-    url: "http://twitter.com/2nf340",
-  },
-  {
-    title: "2nf340",
-    url: "http://2nf340.danhassin.me",
-  },
-  {
-    title: "how i felt in the summer of 2013",
-    url: "stuff/poems.pdf",
-  },
-]
-
 import Page from 'page';
+import * as Papers from 'papers';
 
 import * as Radio from 'radio';
 
@@ -94,11 +42,11 @@ export default class Writing extends Page {
         .content
           %h2 selected university papers
           %ul.paper-table
-            {papers.map(this.renderItem)}
+            {Papers.papers.map(this.renderItem)}
 
           %h2 other
           %span.other-separator {"//"}
-          {other.map(this.renderOtherItem)}
+          {Papers.other.map(this.renderOtherItem)}
     ~)
   }
 }
